ec88c7e10d4b63b07aebaaa868f7936861214dc5
tainafer04 <163296424+tainafer04@users.noreply.github.com>
2025-03-21 10:16:45 -0300

    Add files via upload

---
+++ Java-Petshop-main/CadastroDeAmais/src/br/com/petshop/Cachorro.java
@@ -0,0 +1,19 @@
+package br.com.petshop;
+
+public class Cachorro extends Animal {
+	private String raca;
+	
+	public Cachorro(String nome, int idade, String raca) {
+		super (nome, idade);
+		this.raca = raca;
+	}
+
+	public void latir( ) {
+		System.out.println(getNome() + " está latindo: Au Au! ");
+	}
+	
+	public void exibirInfo( ) {
+		super.exibirInfo();
+		System.out.println("Raça: " + raca); 
+	}
+}
